fa1458e537a6ad3e64ace8cb969cb6acb7a0d245
giselakayy <giselakottmeier@gmail.com>
2015-04-15 16:40:53 -0700

    Shows Json file data on screen

--- arena_reconstruction/views.py
+++ arena_reconstruction/views.py
@@ -1,10 +1,17 @@
 from arena_reconstruction.models import Course, Class, Teacher
 from django.shortcuts import render
 import logging
+import json
 
-courseId=1
+from pprint import pprint
+
+# with open('arena_reconstruction/fixtures/fakeData.json') as data_file:    
+# 		data = json.loads(data_file.read())
+# 		for i in data:
+# 			printIt=Course.o
+# courseId=1
 def showClass(request):
-	showclasses=Class.objects.all().filter(course=courseId)
-	# showclasses="hi"
-	return render (request,"temp.html",{"classes":showclasses})
 
+	# showSelectedCourse=Class.objects.all().filter(course=courseId)
+		# showclasses="hi"
+	return render (request,"temp.html",{"classes":Class.objects.all()})
